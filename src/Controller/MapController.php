6c7f88debfa384183d430393d00d51aa452c46d8
Barbara Carmé <barbara_carme@yahoo.fr>
2019-12-18 21:24:37 +0100

    map open street and markers

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MapController extends AbstractController
 {
     /**
