da38ef01aaf6ab16ba30bba05c059e1610664c7d
Florentin Wilfried <florentinwilfried@gmail.com>
2019-12-19 13:59:38 +0100

    creation de entity et repository picture et le controleur et base de donnee

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use App\Repository\LineRepository;
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,20 +17,23 @@ class DefaultController extends AbstractController
      * @Route("/", name="app_index")
      * @param CategoryRepository $categoryRepository
      * @param LineRepository $lineRepository
+     * @param PictureRepository $pictureRepository
      * @return Response
      */
 
     public function index(
         CategoryRepository $categoryRepository,
-        LineRepository $lineRepository
+        LineRepository $lineRepository,
+        PictureRepository $pictureRepository
     ) {
         $categories = $categoryRepository->findAll();
         $lines = $lineRepository->findAll();
-
+        $picture = $pictureRepository->findAll();
 
         return $this->render('Page_Keodex/keodex.html.twig', [
             'categories' => $categories,
-            'lines' => $lines
+            'lines' => $lines,
+            'picture' => $picture
         ]);
        }
 }
---
+++ src/Entity/Picture.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $display;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Line", inversedBy="pictures")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $line;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getDisplay(): ?bool
+    {
+        return $this->display;
+    }
+
+    public function setDisplay(bool $display): self
+    {
+        $this->display = $display;
+
+        return $this;
+    }
+
+    public function getLine(): ?Line
+    {
+        return $this->line;
+    }
+
+    public function setLine(?Line $line): self
+    {
+        $this->line = $line;
+
+        return $this;
+    }
+}
